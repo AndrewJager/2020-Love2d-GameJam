80c471a4a91cf3f10a16e454eefb4731ec90f61d
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2020-02-23 15:10:11 -0500

    Create menu screen

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 
 local game = {}
-game.level = "stars"
-game.curLevel = "stars"
+game.level = "menu"
+game.curLevel = "menu"
 game.width = 1200
 game.threeSixtyToWidth = game.width / 360
 game.btnFont = love.graphics.newFont(24)
@@ -10,18 +10,35 @@ game.textFont = love.graphics.newFont(18)
 game.textFontSmall = love.graphics.newFont(12)
 
 local space = require("space")
+local menu = require("menu")
 
 function love.load()
     love.window.setTitle("Unnamed")
     love.window.setMode(game.width, 700)
     math.randomseed(os.time())
-    space.load(game)
+    menu.load()
 end
 
 function love.update(dt)
-    space.update(dt, game)
+    if game.level ~= game.curLevel then 
+        if game.level == "stars" then 
+            space.load(game)
+        elseif game.level == "menu" then 
+            menu.load()
+        end
+        game.curLevel = game.level
+    end
+    if game.level == "stars" then 
+        space.update(dt, game)
+    elseif game.level == "menu" then
+        menu.update(dt, game)
+    end
 end
 
 function love.draw()
-    space.draw(game)
+    if game.level == "stars" then 
+        space.draw(game)
+    elseif game.level == "menu" then 
+        menu.draw()
+    end
 end
---
+++ menu.lua
@@ -0,0 +1,80 @@
+local menu = {}
+uare = require "Libraries/uare"
+
+menu.stars = {}
+menu.time = 0
+menu.selectedStar = 5
+menu.starTime = 0.6
+menu.font = love.graphics.newFont("fonts/TradeWinds-regular.ttf", 120)
+menu.fontSmall = love.graphics.newFont("fonts/Inconsolata-regular.ttf", 24)
+
+local keyDown = 0
+
+local function makeDiamond(width, height)
+    local w = width
+    local h = height
+    local poly = {}
+    poly = {
+    0 - (w/2), 0,
+    0, 0 - (h/2),
+    0 + (w/2), 0,
+    0, 0 + (h/2),
+    }
+    return poly
+end
+
+local function starDrawPoly(shape, point)
+    result = {shape[1], shape[2], shape[3], shape[4],
+    shape[5], shape[6], shape[7], shape[8]}
+    result[1] = result[1] + point[1]
+    result[2] = result[2] + point[2]
+    result[3] = result[3] + point[1]
+    result[4] = result[4] + point[2]
+    result[5] = result[5] + point[1]
+    result[6] = result[6] + point[2]
+    result[7] = result[7] + point[1]
+    result[8] = result[8] + point[2]
+    return result
+end
+
+local function load()
+    for i = 1, 100 do 
+        table.insert(menu.stars,{math.random(0, 1200), math.random(0, 700)} )
+    end
+end
+menu.load = load 
+
+function love.keypressed(key, unicode)
+    keyDown = keyDown + 1
+end
+
+local function update(dt, game)
+    menu.time = menu.time + dt
+    if menu.time > menu.starTime then 
+        menu.selectedStar = math.random( 1, #menu.stars )
+        menu.starTime = math.random( 0.6, 2.0 )
+        menu.time = 0
+    end
+    if keyDown > 0 then 
+        game.level = "stars"
+    end
+end
+menu.update = update 
+
+local function draw()
+    local poly = makeDiamond(4, 10)
+    love.graphics.setColor(0.80,0.93,1.00)
+    for i = 1,  #menu.stars do 
+        local drawThis = starDrawPoly(poly, {menu.stars[i][1], menu.stars[i][2]})
+        love.graphics.polygon("fill", drawThis)
+    end
+    love.graphics.rectangle("line", menu.stars[menu.selectedStar][1] - 15 / 2, menu.stars[menu.selectedStar][2] - 15 / 2, 15, 15)
+
+    love.graphics.setFont(menu.font)
+    love.graphics.print("Lonely Skies", 200, 50)
+    love.graphics.setFont(menu.fontSmall)
+    love.graphics.print("Press any key to start", 460, 550)
+end
+menu.draw = draw 
+
+return menu
